8e9699394b1d98de09eddd574e9b005053de7f4e
AndreyTara <explorituse@gmail.com>
2024-08-12 01:30:42 +0300

    'empty-fix'

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,37 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contacts/contactsSlice.js";
 import { filterReducer } from "./contacts/filtersSlice.js";
 
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
-
-// const persistConfig = {
-//   key: "root",
-//   version: 1,
-//   storage,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, contactsReducer);
-
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filter: filterReducer,
   },
-  // middleware: (getDefaultMiddleware) =>
-  //   getDefaultMiddleware({
-  //     serializableCheck: {
-  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-  //     },
-  //   }),
 });
-
-// export let persistor = persistStore(store);
